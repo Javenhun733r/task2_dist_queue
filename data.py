6d475af6acfd7397184f9a840728a8223eb27d67
c1own-depressed <117154948+c1own-depressed@users.noreply.github.com>
2022-11-09 19:49:53 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,23 @@
-import json
-import http
-class Data_node:
-    def get_stats(self):
-        pass
-    def read(self):
-        pass
-    def write(self):
-        pass
-    def resend(self):
-        pass
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+my_data = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():    
+    if request.method == 'GET':
+        return jsonify(isError= False,
+                message= "Success",
+                statusCode= 200,
+                data= my_data,
+                lenght= len(my_data))
+
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        my_data.append(data)
+        return jsonify(data)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(port = '5001')
